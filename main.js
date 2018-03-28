53d1917d6d291a610f330d36f36cddb47c360da0
choilm <choilmto@gmail.com>
2018-03-27 22:40:39 -0400

    Populate buttons and update display.

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 //global components
 Vue.component('button-draw', {
-  data: function() {
-    return {};
-  },
-  template: '<button></button>'
+  props: ['content'],
+  template: '<button v-on:click="$emit(\'press\', content)">{{content}}</button>'
 });
 
 var app = new Vue ({
   el: '#app',
   data: {
+    inputs: [
+      {content: '1'},
+      {content: '2'},
+      {content: '3'},
+      {content: '4'},
+      {content: '5'},
+      {content: '6'},
+      {content: '7'},
+      {content: '8'},
+      {content: '9'},
+      {content: '0'},
+      {content: '+'},
+      {content: '-'},
+      {content: '*'},
+      {content: '/'},
+      {content: '='},
+      {content: 'AC'}
+    ],
+    result: "0"
   }
 });
