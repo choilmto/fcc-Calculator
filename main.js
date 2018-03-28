2e5e50effc277bd7c1cd542119d3ec5ba8f5ef4f
choilm <choilmto@gmail.com>
2018-03-27 22:40:39 -0400

    Initialise Vue and start a button component

---
+++ main.js
@@ -0,0 +1,13 @@
+//global components
+Vue.component('button-draw', {
+  data: function() {
+    return {};
+  },
+  template: '<button></button>'
+});
+
+var app = new Vue ({
+  el: '#app',
+  data: {
+  }
+});
