379bab568051d1119fd296350c630c1ee922b446
choilm <choilmto@gmail.com>
2018-03-27 22:40:39 -0400

    Invokes callback on button-press.

--- main.js
+++ main.js
@@ -1,30 +1,35 @@
 //global components
 Vue.component('button-draw', {
   props: ['content'],
-  template: '<button v-on:click="$emit(\'press\', content)">{{content}}</button>'
+  template: '<button v-on:click="$emit(\'press\')">{{content}}</button>'
 });
+
+var updateDisplay = function (){};
+var useEval = function (){};
+var clearDisplay = function (){};
 
 var app = new Vue ({
   el: '#app',
   data: {
     inputs: [
-      {content: '1'},
-      {content: '2'},
-      {content: '3'},
-      {content: '4'},
-      {content: '5'},
-      {content: '6'},
-      {content: '7'},
-      {content: '8'},
-      {content: '9'},
-      {content: '0'},
-      {content: '+'},
-      {content: '-'},
-      {content: '*'},
-      {content: '/'},
-      {content: '='},
-      {content: 'AC'}
+      {content: '1', processInput: updateDisplay},
+      {content: '2', processInput: updateDisplay},
+      {content: '3', processInput: updateDisplay},
+      {content: '4', processInput: updateDisplay},
+      {content: '5', processInput: updateDisplay},
+      {content: '6', processInput: updateDisplay},
+      {content: '7', processInput: updateDisplay},
+      {content: '8', processInput: updateDisplay},
+      {content: '9', processInput: updateDisplay},
+      {content: '0', processInput: updateDisplay},
+      {content: '.', processInput: updateDisplay},
+      {content: '+', processInput: updateDisplay},
+      {content: '-', processInput: updateDisplay},
+      {content: '*', processInput: updateDisplay},
+      {content: '/', processInput: updateDisplay},
+      {content: '=', processInput: useEval},
+      {content: 'AC', processInput: clearDisplay}
     ],
-    result: "0"
+    result: "0",
   }
 });
