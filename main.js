1f36171d84f94a78f4fc3ba0e2d0abec42584b92
choilm <choilmto@gmail.com>
2018-03-27 22:40:39 -0400

    Refactor using token array, operator set, and objects
    Needs testing.
    
    Known bug: type '4+4', get '4+44'.

--- main.js
+++ main.js
@@ -1,93 +1,139 @@
+const operatorSet = {
+  "+": "+",
+  "-": "-",
+  "/": "/",
+  "*": "*"
+};
+
+function FloatObject (float, flags) {
+  this.displayPart = float;  //should be in string form
+  this.isAnswer = flags.isAnswer;
+}
+
+function OperatorObject (operator) {
+  this.displayPart = operator;
+}
+
+var appendAnswer = function (tokens, answer, isWaitingForFirstInputAfterEquals) {
+  if (isWaitingForFirstInputAfterEquals) {
+    tokens.pop();
+  }
+  if (isWaitingForFirstInputAfterEquals || (tokens[tokens.length - 1] instanceof FloatObject)) {
+    tokens.push(new FloatingObject("", {isAnswer: false}));
+  }
+  if (tokens[tokens.length - 1].displayPart === "") {
+    tokens.display = answer;
+  }
+  return tokens;
+}
+
+var appendOperator = function (tokens, operator, isWaitingForFirstInputAfterEquals) {
+  if (tokens[tokens.length - 1] instanceof FloatObject) {
+    tokens.push(new OperatorObject(""));
+  }
+  tokens[tokens.length - 1].displayPart = operator;
+  return tokens;
+}
+
+var appendContent = function (tokens, content, isWaitingForFirstInputAfterEquals) {
+  if (isWaitingForFirstInputAfterEquals || tokens[tokens.length - 1] instanceof OperatorObject) {
+    tokens.push(new FloatObject(content, {isAnswer: false}));
+  } else {
+    if ((content === ".") && /\./.test(tokens[tokens.length - 1].displayPart)) {
+      return tokens;
+    }
+    if ((content !== ".") && (tokens[tokens.length - 1].displayPart === "0")) {
+      tokens[tokens.length - 1].displayPart = "";
+    }
+  }
+  tokens[tokens.length - 1].displayPart += content;
+  return tokens;
+};
+
+function filteredResultForEval (tokens) {
+  if (tokens.every(function (currentValue) {
+    if (currentValue instanceof FloatObject) {
+      return (currentValue.displayPart === "Infinity") || (Number.parseFloat(currentValue.displayPart).toString() === currentValue.displayPart);
+    }
+    return (currentValue instanceof OperatorObject) && (currentValue.displayPart in operatorSet);
+  })) {
+    return tokens.reduce((accumulator, currentValue) => {
+      if (currentValue instanceof FloatObject) {
+        return accumulator + currentValue.displayPart;
+      }
+      if (currentValue instanceof OperatorObject) {
+        return accumulator + operatorSet[currentValue.displayPart];
+      }
+      throw "Error";
+    }, "");
+  }
+  throw "Error";
+}
+
+var useEval = function (tokens) {
+  try {
+    //console.log(tokens);
+    return [new FloatObject(eval(filteredResultForEval(tokens)), {isAnswer: true})];
+  }
+  catch(e) {
+    window.alert(e);
+    return clearDisplay();
+  }
+};
+
+var clearDisplay = function () {
+  return [new FloatObject("0", {isAnswer: false})];
+};
+
+var inputsArray = (function () {
+  var arrayButtons = [{content: 'AC', processInput: clearDisplay},
+    {content: '=', processInput: useEval},
+    {content: 'ANS', processInput: appendAnswer}
+  ];
+
+  for (var i = 0; i <= 9; i++) {
+    arrayButtons.push({content: i.toString(), processInput: appendContent});
+  }
+
+  for (operators in operatorSet) {
+    arrayButtons.push({content: operators, processInput: appendOperator});
+  }
+  return arrayButtons;
+})();
+
 Vue.component('button-draw', {
   props: ['content'],
   template: '<button v-on:click="$emit(\'press\')">{{content}}</button>'
 });
 
-function adjustContent (result, content) {
-  const lastEntryHasDecimalPoint = /\.\d*$/;
-  const operatorsAtEnd = /[\+\-\*\/]$/;
-  if (operatorsAtEnd.test(content) && operatorsAtEnd.test(result[result.length - 1])) {
-    return "";
-  }
-  if (lastEntryHasDecimalPoint.test(content) && lastEntryHasDecimalPoint.test(result)) {
-    return "";
-  }
-  return content;
-}
-
-function adjustResult (result, content, answer) {
-  const lastEntry = /[\d\.]*$/;
-  if (answer && Number.isInteger(parseInt(content))) {
-    return "";
-  }
-  if ((lastEntry.exec(result)[0] === "0") && Number.isInteger(parseInt(content))){
-    return result.substr(0, result.length - 1);
-  }
-  return result;
-}
-
-var appendContent = function (result, content, answer) {
-  return adjustResult(result, content, answer) + adjustContent(result, content);
-};
-
-function filteredResultForEval (result) {
-  const allowedChars = /e\+|[\d\.\+\-\*\/]|Infinity/g;
-  if (result.match(allowedChars).join("") === result) {
-    return result;
-  }
-  return 0;
-}
-
-var useEval = function (result) {
-  try {
-    return {result: eval(filteredResultForEval(result)),
-      answer: true};
-  }
-  catch(e) {
-    window.alert(e);
-    return "0";
-  }
-};
-
-var clearDisplay = function () {
-  return "0";
-};
-
 var app = new Vue ({
   el: '#app',
   data: {
-    inputs: [
-      {content: '1', processInput: appendContent},
-      {content: '2', processInput: appendContent},
-      {content: '3', processInput: appendContent},
-      {content: '4', processInput: appendContent},
-      {content: '5', processInput: appendContent},
-      {content: '6', processInput: appendContent},
-      {content: '7', processInput: appendContent},
-      {content: '8', processInput: appendContent},
-      {content: '9', processInput: appendContent},
-      {content: '0', processInput: appendContent},
-      {content: '.', processInput: appendContent},
-      {content: '+', processInput: appendContent},
-      {content: '-', processInput: appendContent},
-      {content: '*', processInput: appendContent},
-      {content: '/', processInput: appendContent},
-      {content: '=', processInput: useEval},
-      {content: 'AC', processInput: clearDisplay}
-    ],
-    result: "0",
+    inputs: inputsArray,
+    tokens: [new FloatObject("0", {isAnswer: false})],
+    isWaitingForFirstInputAfterEquals: false,
     answer: ""
   },
+  computed: {
+    display: function () {
+      return this.tokens.reduce((accumulator, currentValue) => accumulator + currentValue.displayPart, "");
+    }
+  },
   methods: {
-    updateDisplay: function (input) {
-      var resultOfProcessInput = input.processInput(this.result, input.content, this.answer);
-      if (typeof resultOfProcessInput === 'object') {
-        this.answer = resultOfProcessInput.answer;
-        resultOfProcessInput = resultOfProcessInput.result;
-      } else {
+    respondToInput: function (input) {
+      if (input.content === "ANS") {
+        input.content = this.answer;
+      }
+      this.tokens = input.processInput(this.tokens, input.content, this.isWaitingForFirstInputAfterEquals);
+      if ((this.tokens[this.tokens.length - 1].isAnswer) && (input.content === "=")) {
+        this.answer = this.tokens[this.tokens.length - 1].displayPart;
+        this.isWaitingForFirstInputAfterEquals = true;
+        return;
+      }
+      if (input.content === "AC") {
         this.answer = "";
       }
-      this.result = resultOfProcessInput;
+      this.isWaitingForFirstInputAfterEquals = false;
     }
   }
 });
